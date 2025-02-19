fced933d7c9fd955a25f812a8865f600aae0627d
xuhuafeifei <2508020102@qq.com>
2025-02-19 17:36:09 +0800

    feat(converter): 添加 List<List<Integer>> 和 List<List<String>> 类型转换器
    - 新增 ListListIntegerConvertor 类用于转换 List<List<Integer>> 类型
    - 新增 ListListStringConvertor 类用于转换 List<List<String>> 类型
    - 在 ParamType 枚举中添加对应的类型和转换器

---
+++ src/main/java/com/xhf/leetcode/plugin/debug/analysis/converter/convert/ListListStringConvertor.java
@@ -0,0 +1,38 @@
+package com.xhf.leetcode.plugin.debug.analysis.converter.convert;
+
+import com.intellij.openapi.externalSystem.service.execution.NotSupportedException;
+
+public class ListListStringConvertor extends AbstractVariableConvertor {
+    @Override
+    protected String doCpp(String testcase, String variableName) {
+        throw new NotSupportedException("cpp not support to convert List_List_String! test case = " + testcase);
+    }
+
+    @Override
+    protected String doPython(String testcase, String variableName) {
+        throw new NotSupportedException("python not support to convert List_List_String! test case = " + testcase);
+    }
+
+    @Override
+    protected String doJava(String testcase, String variableName) {
+        StringBuilder sb = new StringBuilder();
+        String[] rows = testcase.replace("[[", "").replace("]]", "").split("],\\s*\\[");
+
+        sb.append("List<List<String>> ").append(variableName).append(" = new ArrayList<>();\n");
+
+        for (String row : rows) {
+            // 按列分割
+            String[] values = row.split(",");
+            sb.append(variableName).append("add(").append("Arrays.asList(");
+            for (int j = 0; j < values.length; j++) {
+                sb.append("\"").append(values[j].trim()).append("\"");
+                if (j < values.length - 1) {
+                    sb.append(",");
+                }
+            }
+            sb.append("));\n");
+        }
+        // 返回构建好的Java代码字符串
+        return sb.toString();
+    }
+}
