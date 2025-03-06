d955e2c68fecb85e68418dd0714049e9091de4fc
小航love666 <1531137510@qq.com>
2025-03-06 11:30:40 +0800

    converter工厂修改为枚举方式实现单例

--- src/main/java/com/xhf/leetcode/plugin/debug/analysis/converter/convert/ConverterFactory.java
+++ src/main/java/com/xhf/leetcode/plugin/debug/analysis/converter/convert/ConverterFactory.java
@@ -8,22 +8,24 @@ import java.util.Map;
  *
  * @author feigebuge
  * @email 2508020102@qq.com
+ * 《Effective Java》推荐枚举方式实现单例，不仅能避免多线程同步问题，而且还能防止反序列化重新创建新的对象
  */
-public class ConverterFactory {
-    private static final Map<String, VariableConvertor> string2ConvertorMap = new HashMap<>();
+public enum ConverterFactory {
 
-    // 单例模式
-    private static final ConverterFactory instance = new ConverterFactory();
-    private ConverterFactory() {}
-    public static ConverterFactory getInstance() {
-        return instance;
-    }
+    INSTANCE; // 枚举单例实例
+    private final Map<String, VariableConvertor> string2ConvertorMap; // 之前的方式，虚拟机重排序可能会发生问题
 
-    static {
+    private ConverterFactory() {
+        string2ConvertorMap = new HashMap<>();
+        // 初始化映射表
         for (ParamType value : ParamType.values()) {
             string2ConvertorMap.put(value.getType(), value.getConvertor());
         }
     }
+    public static ConverterFactory getInstance() {
+        return INSTANCE;
+    }
+
 
     /**
      * 通过分析器得到的参数类型, 获取对应的converter
