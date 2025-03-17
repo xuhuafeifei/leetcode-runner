a9ccb4322ed2594ca5eb3c0ad9356ad525257357
小航love666 <1531137510@qq.com>
2025-03-17 23:37:46 +0800

    fix(Algorithm): 补充测试类，完善QuestionCard

--- src/main/java/com/xhf/leetcode/plugin/review/backend/card/QuestionCard.java
+++ src/main/java/com/xhf/leetcode/plugin/review/backend/card/QuestionCard.java
@@ -1,5 +1,7 @@
 package com.xhf.leetcode.plugin.review.backend.card;
 
+import com.xhf.leetcode.plugin.review.backend.test.TestAlgorithmApp;
+
 import java.util.UUID;
 
 /**
@@ -8,8 +10,25 @@ import java.util.UUID;
 public class QuestionCard {
 
     private UUID uuid; // 唯一标识符
-    private String question; // 问题内容
+    private String front, back; // 问题内容
     private Long created; // 创建时间
+
+    /**
+     * 构造函数，用于初始化卡片的所有传入参数
+     * @param uuid 卡片的UUID，用于标识
+     * @param front 卡片的正面文本
+     * @param back 卡片的背面文本
+     * @param created 卡片的创建时间
+     */
+    public QuestionCard(UUID uuid, String front, String back, Long created) {
+        this.uuid = uuid;
+        this.front = front;
+        this.back = back;
+        this.created = created;
+
+        TestAlgorithmApp.getInstance().getCards().put(uuid, this);
+    }
+
 
     /**
      * 获取唯一标识符
@@ -31,16 +50,12 @@ public class QuestionCard {
      * 获取问题内容
      * @return 问题内容
      */
-    public String getQuestion() {
-        return question;
+    public String getFront() {
+        return this.front;
     }
 
-    /**
-     * 设置问题内容
-     * @param question 问题内容
-     */
-    public void setQuestion(String question) {
-        this.question = question;
+    public String getBack() {
+        return this.back;
     }
 
     /**
@@ -57,5 +72,29 @@ public class QuestionCard {
      */
     public void setCreated(Long created) {
         this.created = created;
+    }
+
+    /**
+     * 使用给定的数据创建一张卡片，并生成一个随机的UUID
+     * @param front 卡片的正面文本
+     * @param back 卡片的背面文本
+     */
+    public static void create(String front, String back) {
+        UUID uuid = UUID.randomUUID(); // 生成随机的UUID
+        Long created = System.currentTimeMillis(); // 当前时间作为创建时间
+
+        // 插入数据库
+        //
+        new QuestionCard(uuid, front, back, created);
+        System.out.println("[Cards] 成功本地创建卡片 " + uuid);
+    }
+
+    /**
+     * 通过从数据库和TestAlgorithmApp中的HashMap "cards" 删除来删除卡片
+     */
+    public void delete() {
+        TestAlgorithmApp.getInstance().getCards().remove(this.uuid, this);
+        // 数据库操作
+        System.out.println("[Cards] 成功删除卡片 " + this.uuid);
     }
 }
