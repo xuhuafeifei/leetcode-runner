7520c08e6ab01d63cee4830ac2c3a8eb0f273aad
小航love666 <1531137510@qq.com>
2025-04-01 11:14:03 +0800

    fix(Utils): Test CompilerHome Get.

---
+++ src/main/java/com/xhf/leetcode/plugin/utils/CompilerHomeUtils.java
@@ -0,0 +1,47 @@
+package com.xhf.leetcode.plugin.utils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * @author 文艺倾年
+ */
+public class CompilerHomeUtils {
+
+
+    public static void main(String[] args) {
+        String winCommand = "where java";
+        String otherCommand = "where java";
+
+        String processFilePath = "E:/data";
+
+        String osType = System.getProperty("os.name").toLowerCase();
+
+        String command = osType.contains("win") ? winCommand : otherCommand;
+
+        ProcessBuilder processBuilder = new ProcessBuilder(command.split(" "));
+        processBuilder.directory(new File(processFilePath));
+
+        try {
+            Process process = processBuilder.start();
+            // 读取命令的输出
+            InputStream inputStream = process.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            while((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            // 等待命令执行完成
+            int exitCode = process.waitFor();
+            System.out.println("命令执行完成，退出码：" + exitCode);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
