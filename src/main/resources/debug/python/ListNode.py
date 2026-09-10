146afdf256334d16a4dcbd81d5d0a4302d10f102
xuhuafei <2508020102@qq.com>
2026-09-10 11:13:14 +0800

    fix: ListNode.__human_visible__ 添加环检测
    调试反转链表时, 部分反转状态可能形成环,
    __human_visible__ 用 while head 遍历链表导致死循环,
    P 指令卡死, consume_output 60s 超时后调试断开
    改用快慢指针检测环, 遇环截断输出

--- src/main/resources/debug/python/ListNode.py
+++ src/main/resources/debug/python/ListNode.py
@@ -4,12 +4,18 @@ class ListNode:
         self.next = x
 
     def __human_visible__(self):
-        # 迭代
+        # 快慢指针检测环，防止反转链表调试时死循环
         head = self
+        fast = self
         res = '['
         while head:
             res += str(head.val) + ','
             head = head.next
+            if fast and fast.next:
+                fast = fast.next.next
+            if fast and head is fast:
+                res += '...,'
+                break
         if res[-1] == ',':
             res = res[:-1] + ']'
         return res
